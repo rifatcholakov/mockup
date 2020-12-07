201b9e60e6672e6d02c39babd62d20f450687cbb
Rifat Cholakov <rcholakov@devrix.com>
2020-12-07 13:29:42 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const sassLint = require('gulp-sass-lint');
+const babel = require('gulp-babel');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+const files = {
+    scssPath: 'src/sass/**/[^_]*.?(s)css',
+    jsPath: 'src/scripts/**/*.js',
+    imagesPath: 'src/images/*',
+    htmlPath: './index.html'
+};
+
+function scssTask() {
+    return src(files.scssPath)
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('dist/css'))
+        .pipe(postcss([cssnano()]))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/css'));
+}
+
+function jsTask() {
+    return src(files.jsPath)
+        .pipe(
+            babel({
+                presets: ['@babel/env']
+            })
+        )
+        .pipe(concat('bundle.js'))
+        .pipe(dest('dist/scripts'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('dist/scripts'));
+}
+
+function imagesTask() {
+    return src(files.imagesPath)
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+}
+
+function watchTask() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    watch(
+        [files.scssPath, files.jsPath, files.imagesPath, files.htmlPath],
+        parallel(scssTask, jsTask, imagesTask)
+    ).on('change', browserSync.reload);
+}
+
+exports.default = series(parallel(scssTask, jsTask, imagesTask), watchTask);
---
+++ src/scripts/vendor/sample-library2.js
@@ -0,0 +1,6 @@
+const possible = true;
+if(possible) {
+    console.log('It is possible');
+} else {
+    console.log('It is impossible');
+}
