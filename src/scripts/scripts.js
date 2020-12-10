51f323cfba3272b72242ca615077513c043eb3f7
Rifat Cholakov <rcholakov@devrix.com>
2020-12-10 13:19:13 +0200

    completed section one responsive design

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,16 @@
-console.log('Welcome from the scripts folder :)');
+const mobileMenuIconOpen = document.querySelector(".menu-mobile-icon-open");
+const mobileMenuIconClose = document.querySelector(".menu-mobile-icon-close");
+const siteNavigation = document.querySelector(".site-navigation-main");
+
+mobileMenuIconOpen.addEventListener("click", openNav);
+mobileMenuIconClose.addEventListener("click", closeNav);
+
+function openNav() {
+	siteNavigation.style.width = "100%";
+	mobileMenuIconClose.style.display = "block";
+}
+
+function closeNav() {
+	siteNavigation.style.width = "0%";
+	mobileMenuIconClose.style.display = "none";
+}
